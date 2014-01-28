92c1fb4676374892daa3979854daf0d7de3e5cf7
Anish Basu <abasu1983@gmail.com>
2014-01-27 21:21:37 -0800

    Setup cocoapods and added AFNetworking

--- CurrencyConverter/ViewController.m
+++ CurrencyConverter/ViewController.m
@@ -18,6 +18,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	NSString *url = @"http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.xchange where pair in (\"USDMXN\", \"USDCHF\")&format=json&env=store://datatables.org/alltableswithkeys";
+	NSLog(@"URL = %@", url);
+	NSLog(@"Encoded URL = %@", [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]);
 }
 
 - (void)didReceiveMemoryWarning
