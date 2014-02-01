e0235cf6c8853643b86cf331a70eb4c607e3bdbc
Anish Basu <abasu1983@gmail.com>
2014-02-01 13:13:19 -0800

    Refactored to use object manager and not run in main queue

--- CurrencyConverter/ExchangeRateClient.m
+++ CurrencyConverter/ExchangeRateClient.m
@@ -9,6 +9,11 @@
 #import "ExchangeRateClient.h"
 
 #define APP_ID @"199670ca739e4bafa1393b0a65d167d0"
+#define BASE_URL @"http://openexchangerates.org"
+
+@interface ExchangeRateClient()
+@property (nonatomic, strong) RKObjectManager *objectManager;
+@end
 
 @implementation ExchangeRateClient
 
@@ -23,39 +28,46 @@
 	return  _manager;
 }
 
+- (id)init
+{
+	self = [super init];
+	if (self) {
+		_objectManager = [RKObjectManager managerWithBaseURL:[NSURL URLWithString:BASE_URL]];
+		RKObjectMapping *mapping = [RKObjectMapping mappingForClass:[ExchangeRate class]];
+		[mapping addAttributeMappingsFromDictionary:@{
+													  @"timestamp": @"timestamp",
+													  @"base": @"baseCurrencyCode",
+													  @"rates": @"rates"
+													  }];
+		NSIndexSet *statusCodes = RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful); // Anything in 2xx
+		RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping
+																								method:RKRequestMethodAny
+																						   pathPattern:nil
+																							   keyPath:nil
+																						   statusCodes:statusCodes];
+		[_objectManager addResponseDescriptorsFromArray:@[responseDescriptor]];
+	}
+	return self;
+}
+
 - (void)fetchExchangeRatesWith:(ExchangeRateResponse)response
 {
 	RKObjectRequestOperation *operation = [self requestOperation];
-	[operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *result) {
-		ExchangeRate *rates = [[result array] firstObject];
+	[operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+		ExchangeRate *rates = [[mappingResult array] firstObject];
 		response(rates, nil);
 	} failure:^(RKObjectRequestOperation *operation, NSError *error) {
 		response(nil, error);
 	}];
-	[operation start];
+	[self.objectManager enqueueObjectRequestOperation:operation];
 }
 
 - (RKObjectRequestOperation *)requestOperation
 {
-	RKObjectMapping *mapping = [RKObjectMapping mappingForClass:[ExchangeRate class]];
-	[mapping addAttributeMappingsFromDictionary:@{
-												  @"timestamp": @"timestamp",
-												  @"base": @"baseCurrencyCode",
-												  @"rates": @"rates"
-												  }];
-	NSIndexSet *statusCodes = RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful); // Anything in 2xx
-	RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping
-																							method:RKRequestMethodAny
-																					   pathPattern:nil
-																						   keyPath:nil
-																					   statusCodes:statusCodes];
-	NSString *urlString = [NSString stringWithFormat:@"https://openexchangerates.org/api/latest.json?app_id=%@", APP_ID];
-	NSURL *url = [NSURL URLWithString:urlString];
-	NSURLRequest *request = [NSURLRequest requestWithURL:url];
-	RKObjectRequestOperation *operation = [[RKObjectRequestOperation alloc] initWithRequest:request
-																		responseDescriptors:@[responseDescriptor]];
-	
-	return operation;
+	return [self.objectManager appropriateObjectRequestOperationWithObject:nil
+																	method:RKRequestMethodGET
+																	  path:@"/api/latest.json"
+																parameters:@{@"app_id": APP_ID}];
 }
 
 @end
